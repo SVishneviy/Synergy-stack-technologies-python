ee92398ce83b84fca8ab871fd3218d850b97f885
SVishneviy <onlainer2010@gmail.com>
2023-08-31 08:17:01 +0400

    Logical and conditional operators homework

--- logical and conditional operators/Homework_2.py
+++ logical and conditional operators/Homework_2.py
@@ -1,14 +1,17 @@
 print("Введите слово на латинице:")
 str = input().lower()
-if (str.find("a") != -1 
-    and str.find("e") != -1 
-    and str.find("i") != -1 
-    and str.find("o") != -1 
-    and str.find("u") != -1):
-    print("Количество a: %s" %str.count("a"))
-    print("Количество e: %s" %str.count("e"))
-    print("Количество i: %s" %str.count("i"))
-    print("Количество o: %s" %str.count("o"))
-    print("Количество u: %s" %str.count("u"))
+str1 = str.translate({ord('a') : None, ord('e') : None, ord('i') : None, ord('o') : None, ord('u') : None})
+print(f"Количество гласных: {str.count('a') + str.count('e') + str.count('i') + str.count('o') + str.count('u')}")
+print(f"Количество согласных: {len(str1)}")
+if (str.find("a") != -1 and 
+    str.find("e") != -1 and 
+    str.find("i") != -1 and 
+    str.find("o") != -1 and 
+    str.find("u") != -1):
+    print(f"Количество a: {str.count('a')}")
+    print(f"Количество e: {str.count('e')}")
+    print(f"Количество i: {str.count('i')}")
+    print(f"Количество o: {str.count('o')}")
+    print(f"Количество u: {str.count('u')}")
 else:
     print(False)
